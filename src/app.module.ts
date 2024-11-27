7472ee60ba0caa9586218942ae6975df780cf6d9
Matheus Dias <hmatheus031@gmail.com>
2024-11-26 23:14:19 -0300

    feat: add initial database setup with PostgreSQL and user/task entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TaskController } from './task/task.controller';
+import { TaskModule } from './task/task.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TaskController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TaskModule,
+    UsersModule,
+    AuthModule,
+    DbModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
